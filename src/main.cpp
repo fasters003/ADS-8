37a2833bb9a2baca4d8c2a0f044ef484d4cfe240
fasters003 <andrey1990ver@gmail.com>
2026-05-19 17:51:14 +0300

    Add tree creation and frequency printing functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
+    BST<std::string> tree;
+    const char* filename = "src/war_peace.txt"; 
+
+    makeTree(tree, filename);
+    
+    std::cout << "Depth: " << tree.depth() << std::endl;
+    printFreq(tree);
+
     return 0;
 }
